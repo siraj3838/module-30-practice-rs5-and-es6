6e466257e65da90900ac3ba6ed5c3e56eb3e33c3
Sirajul Islam <ayasabegum134@gmail.com>
2023-08-23 17:04:50 +0600

    my happy commit

--- importan.js
+++ importan.js
@@ -167,30 +167,66 @@
 
 
 
-const searchThird = number => number[2];
-const third = searchThird([5, 7, 44, 77, 46])
-console.log(third);
+// const searchThird = number => number[2];
+// const third = searchThird([5, 7, 44, 77, 46])
+// console.log(third);
 
 
-const double = num => num * 2;
-const doubleNum = double(10);
-console.log(doubleNum);
+// const double = num => num * 2;
+// const doubleNum = double(10);
+// console.log(doubleNum);
 
 
-const getPI = () => Math.PI
-console.log(getPI())
+// const getPI = () => Math.PI
+// console.log(getPI())
 
-// multiple line calculation so use return 
-const doMath = (x, y, z) => {
-    const sum = x + y + z;
-    const multi = x * y * z;
-    const result = sum + multi;
-    console.log(sum, multi)
-    return result;
-}
-const number2 = doMath(4, 6, 8);
-console.log(number2);
-
-
+// // multiple line calculation so use return 
+// const doMath = (x, y, z) => {
+//     const sum = x + y + z;
+//     const multi = x * y * z;
+//     const result = sum + multi;
+//     console.log(sum, multi)
+//     return result;
+// }
+// const number2 = doMath(4, 6, 8);
+// console.log(number2);
 
 
+// max number
+const maxNum = Math.max(30,789,644,4232,2234,5656,70)
+console.log(maxNum);
+
+const number = [10, 4, 234, 12, 123, 12, 76];
+// not a number NaN 
+const arrayNum = Math.max(number);
+console.log(arrayNum);
+
+// but problem solve if use Math.max(...number);
+const numberMax = Math.max(...number); 
+console.log(numberMax);
+
+// jodi number2 namer array theke sob number Element alada korte chai taholeo three dot (...number2) dibo
+
+const number2 = [12, 40, 50, 34, 67];
+console.log(...number2);
+
+
+// spread operator to copy
+
+// const nums1 = [6, 2, 8, 3, 8]
+// const nums2 = nums1;
+// nums2.push(22);
+// console.log(nums1);
+// console.log(nums2);
+
+const nums1 = [6, 2, 8, 3, 8]
+const nums2 = nums1;
+const final = [...nums1]  // copy
+console.log(final);
+nums2.push(30);
+console.log(final);
+console.log(nums1)
+
+// advance
+const newNum = [...nums1, 44]; //copy and set or add
+console.log(newNum)
